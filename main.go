8ccffd1c398c9fef2c2c56b92af647957d042fe9
Chisa Sichali <chisa.sichali@dotcomzambia.com>
2026-08-19 09:25:18 +0200

    feat(Handlers): - Added handlers.go to handle short url creation and redirection logic

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"go-url-shortener/handler"
+	"go-url-shortener/store"
 
 	"github.com/gin-gonic/gin"
 )
@@ -13,6 +15,16 @@ func main() {
 			"message": "Hey Go URL Shortener !",
 		})
 	})
+
+	r.POST("/create-short-url", func(c *gin.Context) {
+		handler.CreateShortUrl(c)
+	})
+
+	r.GET("/:shortUrl", func(c *gin.Context) {
+		handler.HandleShortUrlRedirect(c)
+	})
+
+	store.InitializeStore()
 
 	err := r.Run(":9808")
 	if err != nil {
